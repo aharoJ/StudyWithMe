13077ff5ea019071c849f0abee8d8f74657fe705
Angel J. Haro <haro.j.angel@gmail.com>
2024-03-12 19:14:31 -0700

    ...aharoJ

---
+++ LeetCode/BinarySearch/Binary.java
@@ -0,0 +1,14 @@
+
+
+
+
+public class Binary {
+    public static void name() {
+        int[] nums1 = { 1, 3, 5, 6 };
+        System.out.println("Insert position for 5: " + solution.searchInsert(nums1, 5)); // Output: 2
+
+        int[] nums2 = { 1, 3, 5, 6 };
+        System.out.println("Insert position for 2: " + solution.searchInsert(nums2, 2)); // Output: 1
+    }
+
+}
