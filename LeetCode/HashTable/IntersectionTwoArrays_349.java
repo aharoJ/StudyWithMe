2f6ce1d15847c4d0b8a03de344dc372c6e2ed0d2
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-03 18:43:08 -0700

    ...aharoJ

--- LeetCode/HashTable/IntersectionTwoArrays_349.java
+++ LeetCode/HashTable/IntersectionTwoArrays_349.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 public class IntersectionTwoArrays_349 
 {
+    
     public int[] intersection(int[] nums1, int[] nums2) 
     {
         Set<Integer> set= new HashSet<>();
@@ -17,11 +18,12 @@ public class IntersectionTwoArrays_349
         List<Integer> list= new ArrayList<>();
         for (int val: nums2){
             if (set.contains(val)){
-                System.out.print("Val: " + val);
+                System.out.println("Val: " + val);
                 set.remove(val);
                 list.add(val);
             }
         }
+        
         
         int array[]= new int[list.size()];
         for(int i=0; i< list.size(); ++i)
@@ -31,6 +33,13 @@ public class IntersectionTwoArrays_349
         return array;
     }
 
+    public boolean tester(){
+        int does_equal=1;
+        if (does_equal == 1){
+            return true;
+        }
+        return false;
+    }
 
 
     public static void main(String[] args) {
@@ -46,3 +55,4 @@ public class IntersectionTwoArrays_349
     }
     
 }
+
--- LeetCode/HashTable/NumberGoodPairs_1512.java
+++ LeetCode/HashTable/NumberGoodPairs_1512.java
@@ -13,19 +13,20 @@ public class NumberGoodPairs_1512 {
         // return 4 good pairs (0,3), (0,4), (3,4), (2,5)
 
         Map<Integer,Integer> map= new HashMap<>();
-        int counter=0;
+        int counter=0; 
         for (int i=0; i<nums.length; ++i)
         {
             int freq= map.getOrDefault(nums[i], 0);
             map.put(nums[i], freq+1);
             counter+= freq; 
+            ka
         }
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
         }
         return counter;
     }
-
+    
     public static void main(String[] args) {
         NumberGoodPairs_1512 solution= new NumberGoodPairs_1512();
         int[] array={1,2,3,1,1,3};
