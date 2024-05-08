2392d5343a21c5376e3871d6955d9cb66b304748
Angel J. Haro <haro.j.angel@gmail.com>
2024-05-07 18:14:10 -0700

    Leetcode 136

--- leetcode/hashtable/SingleNumber_136.java
+++ leetcode/hashtable/SingleNumber_136.java
@@ -1,4 +1,4 @@
-package leetcode.array;
+package hashtable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,43 +16,23 @@ import java.util.Map;
  * Input: nums = [1]
  * Output: 1
  */
+@SuppressWarnings("unused") // Nvim  
 public class SingleNumber_136 {
+
   public int singleNumber(int[] nums) {
-    Map<Integer, Integer> map = new HashMap<>();
+    HashMap<Integer, Integer> map = new HashMap<>();
     for (int val : nums) {
       int freq = map.getOrDefault(val, 0);
       map.put(val, freq + 1);
     }
-    int uniqueNum=0;
-    for (int val : nums) {
-      if (map.get(val) == 1) {
-        uniqueNum= val;
+
+    int unique_num = -1;
+    for (int key_freq : map.keySet()) {
+      if (map.get(key_freq) == 1) {
+        unique_num = key_freq;
       }
     }
-    return uniqueNum;
-  }
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
+    return unique_num;
 
-  // public int singleNumber(int[] nums) {
-  // int k = Integer.MAX_VALUE;
-  // for (int i = 1; i < nums.length; i++) {
-  // if (nums[i] != nums[i - 1]) {
-  // k = nums[i];
-  // }
-  // }
-  // return k;
-  // }
+  }
 }
