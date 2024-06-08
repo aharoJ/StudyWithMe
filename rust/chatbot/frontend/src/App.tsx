2a84c9531a350893e9f333c3779bd8dfa8e34131
Angel J. Haro <haro.j.angel@gmail.com>
2024-06-08 15:35:53 -0700

    prep: message, name, vis

--- rust/chatbot/frontend/src/App.tsx
+++ rust/chatbot/frontend/src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { SetStateAction } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+type Message = {
+  name: string;
+  uid: number;
+  message: string;
+};
 
+const App = () => {
+  const [message, setMessgae] = React.useState<string>("");
+  const [name, setName] = React.useState<string>("");
+  const [vis, setVis] = React.useState<boolean>(true);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-row text-gray-100">
+        <div className="w-full bg-slate-700 flex flex-col pb-5">
+          <div
+            className="w-full min-h-screen flex flex-col justify-end gap-4 pb-20"
+            id="chatbox"
+          >
+            <p> Hi! Lets get MF rustyyy</p>
+          </div>
+        </div>
+        <form
+          className="w-full h-10 fixed bottom-0 flex flex-row justify-center gap-4 mb-5 mx-5 "
+          onSubmit={(e) => sendMessage(e, name, message, setMessgae)}
+        >
+          <input
+            name="messgae"
+            id="messageBox"
+            type="text"
+            className="bg-slate-400 w-full py-2 px-5 focus:outline-0 rounded-tl-xl rounded-bl-xl"
+            value={message}
+            placeholder="Enter your message here..."
+            onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setMessgae(e.target.value)
+            }
+          />
+          <button
+            id="messageBtn"
+            className="bg-slate-400 px-2 active:translate-y-0.5 active:translate-x-0.5 hover:text-black transition-all rounded-tr-xl rounded-br-xl"
+          >
+            Send Message
+          </button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
