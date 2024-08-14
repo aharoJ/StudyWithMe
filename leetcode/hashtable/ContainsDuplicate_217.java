4fae94cadc417a960fbe4464fc69a82f40e5c67b
Angel J. Haro <haro.j.angel@gmail.com>
2024-08-13 19:33:27 -0700

    Leetcode: 217

---
+++ leetcode/hashtable/ContainsDuplicate_217.java
@@ -0,0 +1,22 @@
+package hashtable;
+
+/**
+ * Input: nums = [1,2,3,1]
+ * Output: true
+ * 
+ * Input: nums = [1,2,3,4]
+ * Output: false
+ */
+class ContainsDuplicate_217 {
+  public boolean containsDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    for (int vals : nums) {
+      if (!set.contains(vals)) {
+        set.add(vals);
+      } else if (set.contains(vals)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
