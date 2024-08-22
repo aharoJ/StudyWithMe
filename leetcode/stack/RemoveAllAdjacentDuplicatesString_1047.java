a5afd7a028e2d1d61b8574fc131a6b3b542ef353
Angel J. Haro <haro.j.angel@gmail.com>
2024-08-21 20:00:40 -0700

    leetcode 1047: Stack<Char>

---
+++ leetcode/stack/RemoveAllAdjacentDuplicatesString_1047.java
@@ -0,0 +1,25 @@
+package stack;
+
+import java.util.Stack;
+
+/**
+ * RemoveAllAdjacentDuplicatesString_1047
+ */
+class Solution {
+  public String removeDuplicates(String s) {
+    Stack<Character> stack = new Stack<>();
+    for (char c : s.toCharArray()) {
+      if (!stack.isEmpty() && stack.peek() == c) {
+        stack.pop();
+      } else {
+        stack.push(c);
+      }
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (char c : stack) {
+      sb.append(c);
+    }
+    return sb.toString();
+  }
+}
