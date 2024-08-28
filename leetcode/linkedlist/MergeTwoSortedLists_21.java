b1959cc542ddec947c55348ec951e710437b006a
Angel J. Haro <haro.j.angel@gmail.com>
2024-08-27 20:35:03 -0700

    leetcode 21: linkedlist

---
+++ leetcode/linkedlist/MergeTwoSortedLists_21.java
@@ -0,0 +1,32 @@
+package linkedlist;
+
+import youtube.SingleLink.ListNode;
+
+/**
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ * 
+ * Input: list1 = [], list2 = []
+ * Output: []
+ * 
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
+ */
+class Solution {
+  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    if (list1 == null) {
+      return list2;
+    }
+    if (list2 == null) {
+      return list1;
+    }
+
+    if (list1.val < list2.val) {
+      list1.next = mergeTwoLists(list1.next, list2);
+      return list1;
+    } else if (list2.val < list1.val) {
+      list2.next = mergeTwoLists(list1, list2.next);
+      return list2;
+    }
+  }
+}
