96e6067bcc5450b6dc50e4d5955be62f4d055e91
Angel J. Haro <haro.j.angel@gmail.com>
2024-09-05 01:55:59 -0700

    recursion: reverse nums

---
+++ kunal/recursion/ReverseNumber.java
@@ -0,0 +1,22 @@
+/**
+ * ReverseNumber
+ */
+public class ReverseNumber {
+  public static void main(String[] args) {
+    int answer = reverse(1824);
+    System.out.println(answer);
+  }
+
+  static int reverse(int n) {
+    int digits = (int) (Math.log10(n) + 1);
+    return helper(n, digits);
+  }
+
+  static int helper(int n, int digits) {
+    if (n % 10 == n) {
+      return n;
+    }
+    int rem = n % 10;
+    return rem * (int) (Math.pow(10, digits - 1)) + helper(n / 10, digits - 1 ) ;
+  }
+}
