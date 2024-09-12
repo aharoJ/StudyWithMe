28df73af13dd3dc99501b35ed6dbc59d2aa308dc
Angel J. Haro <haro.j.angel@gmail.com>
2024-09-11 23:42:53 -0700

    ll: main

--- kunal/linkedlist/Main.java
+++ kunal/linkedlist/Main.java
@@ -2,15 +2,39 @@ package linkedlist;
 
 public class Main {
   public static void main(String[] args) {
-    LL list = new LL();
+    // LL list = new LL();
+    // list.insertFirst(10);
+    // list.insertFirst(20);
+    // list.insertFirst(30);
+    // list.insertLast(9);
+    // list.insert(69, 3);
+    // list.display();
+    // System.out.println(list.deleteFirst());
+    // list.display();
+    // list.deleteLast();
+    // list.display();
+    // list.delete(1);
+    // list.display();
+    // ------------------------------------------
+    // ------------------------------------------
+    // DLL list = new DLL();
+    // list.insertFirst(3);
+    // list.insertFirst(2);
+    // list.insertFirst(8);
+    // list.insertFirst(17);
+    // list.insertLast(99);
+    // list.insert(69, 2);
+    // list.insertAfter(69, 33);
+    // list.display();
+    // ------------------------------------------
+    // ------------------------------------------
+    CLL list = new CLL();
+    list.inset(23);
+    list.inset(3);
+    list.inset(19);
+    list.inset(75);
+    list.delete(19);
+    list.display();
 
-    // Insert some elements and display the list
-    list.insertFirst(10);
-    list.insertFirst(20);
-    list.insertFirst(30);
-    list.insertLast(9);
-    list.insert(69, 3);
-
-    list.display(); // Output: 30 -> 20 -> 10 -> NULL
   }
 }
