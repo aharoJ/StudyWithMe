e257b54d8af3fcf7fd3a6451484e95ecbf41e5aa
Angel J. Haro <haro.j.angel@gmail.com>
2024-09-11 23:42:45 -0700

    linkedlist: circular

---
+++ kunal/linkedlist/CLL.java
@@ -0,0 +1,72 @@
+package linkedlist;
+
+/**
+ * CLL
+ */
+public class CLL {
+  private Node head;
+  private Node tail;
+  private int size;
+
+  public void inset(int val) {
+    Node node = new Node(val);
+    if (head == null) {
+      head = node;
+      tail = node;
+      return;
+    }
+
+    tail.next = node;
+    node.next = head;
+    tail = node;
+  }
+
+  public void display() {
+    Node node = head;
+    if (head != null) {
+      do {
+        System.out.print(node.value + "--> ");
+        node = node.next;
+      } while (node != head);
+      System.out.println("(back to head)");
+    }
+  }
+
+  public void delete(int val) {
+    Node node = head;
+    if (node == null) {
+      return;
+    }
+    if (node.value == val) {
+      head = head.next;
+      tail.next = head;
+      return;
+    }
+
+    do {
+      Node n = node.next;
+      if (n.value == val) {
+        node.next = n.next;
+        break;
+      }
+      node = node.next;
+    } while (node != head);
+
+  }
+
+  private class Node {
+    private int value;
+    private Node next;
+    private Node prev;
+
+    public Node(int value) {
+      this.value = value;
+    }
+
+    public Node(int value, Node next, Node prev) {
+      this.value = value;
+      this.next = next;
+      this.prev = prev;
+    }
+  }
+}
