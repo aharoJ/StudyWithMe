9059f5548cc3fb29bc45e20e7bac1d9d0fc9f5b9
Angel J. Haro <haro.j.angel@gmail.com>
2024-09-18 11:31:26 -0700

    stack: valid parenthesis

---
+++ kunal/stack/ValidParentheses.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+/**
+ * ValidParentheses
+ */
+public class ValidParentheses {
+  public static void main(String[] args) {
+  }
+
+  public boolean isValid(String s) {
+    Stack<Character> stack = new Stack<>();
+    for (char ch : s.toCharArray()) {
+      if (ch == '(' || ch == '{' || ch == '[') {
+        stack.push(ch);
+      } else {
+        if (ch == ')') {
+          if (stack.isEmpty() || stack.pop() != '(') {
+            return false;
+          }
+        }
+        if (ch == '}') {
+          if (stack.isEmpty() || stack.pop() != '{') {
+            return false;
+          }
+        }
+        if (ch == ']') {
+          if (stack.isEmpty() || stack.pop() != '[') {
+            return false;
+          }
+        }
+      }
+    }
+    return stack.isEmpty();
+  }
+}
