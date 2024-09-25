5f8aa68e694ca6c6fab9034b8d7f7350f4979b60
Angel J. Haro <haro.j.angel@gmail.com>
2024-09-25 01:02:12 -0700

    del

--- typescript/02/build/js/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let username = "aharoJ";
-console.log(username);
-let a = 12;
-let b = 6;
-let c = 2;
-console.log(a / b);
-console.log(c * b);
-console.log(b + c);
--- typescript/watch-store/src/hooks/useCart.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react"
-import CartContext from "../context/CartProvider"
-import { UseCartContexType } from "../context/CartProvider"
-
-const useCart = ():UseCartContexType => {
-  return useContext(CartContext)
-}
-
-export default useCart
