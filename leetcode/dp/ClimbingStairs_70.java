dbd116824a28b3f413bdfbab1fcfb8c1117e2d0b
Angel J. Haro <haro.j.angel@gmail.com>
2024-09-29 01:27:59 -0700

    leetcode 70 | dp: tabulation/memoization

--- leetcode/dp/ClimbingStairs_70.java
+++ leetcode/dp/ClimbingStairs_70.java
@@ -1,23 +1,44 @@
 package dp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  */
 public class ClimbingStairs_70 {
+  private Map<Integer, Integer> memo = new HashMap<>(
+      Map.of(0, 0, 1, 1));
+
   public int climbStairs(int n) {
-    int[] dp = new int[n + 1];
-    dp[1] = 1;
-    dp[2] = 1;
-    for (int i = 3; i <= n; ++i) {
-      dp[i] = dp[i - 1] + dp[i - 2];
-    }
-    return dp[n];
+    // INSERT EITHER memoized(n) OR tabulation(n) HERE
+    return -1;
   }
 
-  public static void main(String[] args) {
-    ClimbingStairs_70 obj = new ClimbingStairs_70();
-    System.out.println(obj.climbStairs(2));
-    System.out.println(obj.climbStairs(3));
-    System.out.println(obj.climbStairs(4));
-    System.out.println(obj.climbStairs(5));
+  public int tabulazation(int n) {
+    if (n <= 1) {
+      return n;
+    }
+    int[] cache = new int[n + 1];
+    cache[0] = 0;
+    cache[1] = 1;
+    cache[2] = 2;
+    for (int i = 3; i <= n; ++i) {
+      cache[i] = cache[i - 1] + cache[i - 2];
+    }
+    return cache[n];
+  }
+
+  public int memoized(int n) {
+    if (n <= 2) {
+      return n;
+    }
+    if (memo.containsKey(n)) {
+      return memo.get(n);
+    }
+
+    int computate = memoized(n - 1) + memoized(n - 2);
+    memo.put(n, computate);
+
+    return computate;
   }
 }
