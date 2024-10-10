439ce0f09ad4b7ba4f97de4ca74243fcf8bfe2ec
Angel J. Haro <haro.j.angel@gmail.com>
2024-10-10 10:38:56 -0700

    temp push: binarysearch

--- leetcode/binarysearch/KokoEatingBananas_875.java
+++ leetcode/binarysearch/KokoEatingBananas_875.java
@@ -1,6 +1,12 @@
 package binarysearch;
 
 /**
+ * round up --> (banana / K) 3.01 --> 4
+ *
+ * banana: 3, 6, 7, 11
+ * k:4---> 1, 2, 2, 3
+ * h:----> 7, 5, 3, 0
+ * 
  * Input: piles = [3,6,7,11], h = 8
  * Output: 4
  * 
@@ -9,26 +15,28 @@ package binarysearch;
  */
 public class KokoEatingBananas_875 {
   public int minEatingSpeed(int[] piles, int h) {
-    int left = 1, right = 1;
+    int left = 1;
+    int right = 1; // place holder
     for (int pile : piles) {
       right = Math.max(right, pile);
     }
 
     while (left < right) {
-      int mid = left + (right - left) / 2;
-      int hours_eating = 0;
-
-      for (int pile : piles) {
-        hours_eating += Math.ceil((double) pile / mid);
-
-        if (hours_eating >= h) {
-          right = mid;
-        } else {
-          left = mid + 1;
-        }
+      int k = left + (right - left) / 2;
+      if (canFinish(piles, k, h)) {
+        right = k;
+      } else {
+        left = k + 1;
       }
     }
-    return left;
+    return right;
+  }
 
+  private boolean canFinish(int[] piles, int k, int h) {
+    int hours = 0;
+    for (int pile : piles) {
+      hours += Math.ceil((double) pile / k);
+    }
+    return hours <= h;
   }
 }
--- leetcode/binarysearch/Solution.java
+++ leetcode/binarysearch/Solution.java
@@ -1,9 +1,36 @@
 package binarysearch;
 
 /**
- * Solution
+ * round up --> (banana / K) 3.01 --> 4
+ *
+ * banana: 3, 6, 7, 11
+ * k:4---> 1, 2, 2, 3
+ * h:----> 7, 5, 3, 0
  */
-public class Solution {
+class Solution {
+  public int minEatingSpeed(int[] piles, int h) {
+    int left = 1;
+    int right = 1; // place holder
+    for (int pile : piles) {
+      right = Math.max(right, pile);
+    }
 
-  
+    while (left < right) {
+      int k = left + (right - left) / 2;
+      if (canFinish(piles, k, h)) {
+        right = k;
+      } else {
+        left = k + 1;
+      }
+    }
+    return right;
+  }
+
+  private boolean canFinish(int[] piles, int k, int h) {
+    int hours = 0;
+    for (int pile : piles) {
+      hours += Math.ceil((double) pile / k);
+    }
+    return hours <= h;
+  }
 }
